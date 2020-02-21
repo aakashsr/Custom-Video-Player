a29d98458a07308ae419450d5c45d4ce37eb97f7
aakashsr <aakashsri.dev@gmail.com>
2020-02-21 23:52:36 +0530

    added functionaity for progress bar , to set progress bar

--- script.js
+++ script.js
@@ -53,6 +53,34 @@ function stopVideo() {
     video.pause();
 }
 
+// update progress & timestamp
+function updateProgress() {
+    console.log('updateprogress');
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    // Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    // Get seconds 
+
+    let secs = Math.floor(video.currentTime % 60);
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+// set video time to progress 
+
+function setVideoProgress() {
+    console.log('setvideoprogress');
+    video.currentTime = (+progress.value * video.duration) / 100;
+}
+
 
 
 
@@ -62,10 +90,7 @@ function stopVideo() {
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
-// video.addEventListener('timeupdate', updateProgress);
-
+video.addEventListener('timeupdate', updateProgress);
 play.addEventListener('click', toggleVideoStatus);
-
 stop.addEventListener('click', stopVideo)
-
-// progress.addEventListener('change', setVideoProgress)
+progress.addEventListener('change', setVideoProgress)
